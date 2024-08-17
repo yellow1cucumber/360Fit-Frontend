252786a70915726b70eb828f21bc482032fda557
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-17 18:20:04 +0500

    FEAT: add application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HeaderComponent} from "./Components/header/header.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "/", component: HeaderComponent},
+  {path: "clients", component: HeaderComponent},
+  {path: "service", component: HeaderComponent},
+  {path: "storage", component: HeaderComponent},
+  {path: "nomenclature", component: HeaderComponent},
+  {path: "staff", component: HeaderComponent},
+  {path: "taxes", component: HeaderComponent},
+  {path: "reports", component: HeaderComponent},
+];
