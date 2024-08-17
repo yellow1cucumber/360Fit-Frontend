2ab37992429118d1cbcac9a3d71e3164472c7021
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-17 18:12:46 +0500

    FEAT: add nav link

--- src/app/Components/nav-link/nav-link.component.ts
+++ src/app/Components/nav-link/nav-link.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-nav-link',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './nav-link.component.scss'
 })
 export class NavLinkComponent {
-
+  @Input() link: string;
+  @Input() text: string;
 }
