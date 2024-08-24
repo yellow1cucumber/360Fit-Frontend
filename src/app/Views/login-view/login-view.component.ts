11e3e4ae14844f4f0582fa15ecd499df0555b451
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-24 21:19:33 +0500

    FEAT: connect template with component logic

--- src/app/Views/login-view/login-view.component.ts
+++ src/app/Views/login-view/login-view.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login-view',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    FormsModule
   ],
   templateUrl: './login-view.component.html',
   styleUrl: './login-view.component.scss'
 })
 export class LoginViewComponent {
-
+  public LoginForm = {
+    login: '',
+    password: ''
+  }
 }
