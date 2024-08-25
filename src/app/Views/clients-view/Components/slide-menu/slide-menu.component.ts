7f4e3567b2ecd29fdbd0c1b602f0695e9469388a
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-25 23:15:48 +0500

    FEAT: add user edit/create slide menu

---
+++ src/app/Views/clients-view/Components/slide-menu/slide-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './slide-menu.component.html',
+  styleUrl: './slide-menu.component.scss'
+})
+export class SlideMenuComponent {
+
+}
