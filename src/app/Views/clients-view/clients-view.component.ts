a381738252557d44f05a5479dcc79cca53fcfaf4
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-25 23:55:03 +0500

    FEAT: add inputs

--- src/app/Views/clients-view/clients-view.component.ts
+++ src/app/Views/clients-view/clients-view.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {ClientsTableComponent} from "./Components/clients-table/clients-table.component";
 import {HeaderComponent} from "../../Components/header/header.component";
 import {UserSearchComponent} from "./Components/user-search/user-search.component";
+import {SlideMenuComponent} from "./Components/slide-menu/slide-menu.component";
 
 @Component({
   selector: 'app-clients-view',
@@ -9,7 +10,8 @@ import {UserSearchComponent} from "./Components/user-search/user-search.componen
   imports: [
     ClientsTableComponent,
     HeaderComponent,
-    UserSearchComponent
+    UserSearchComponent,
+    SlideMenuComponent
   ],
   templateUrl: './clients-view.component.html',
   styleUrl: './clients-view.component.scss'
