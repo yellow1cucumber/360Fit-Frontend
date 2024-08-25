ee604ef8e70b9de2be0b78bb969a81e81359b07c
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-26 03:03:59 +0500

    FIX: add button logic

--- src/app/Views/clients-view/Components/slide-menu/slide-menu.component.ts
+++ src/app/Views/clients-view/Components/slide-menu/slide-menu.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-slide-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './slide-menu.component.html',
   styleUrl: './slide-menu.component.scss'
 })
 export class SlideMenuComponent {
 
+  public IsActive = false;
+
+  public ChangeVisibility(): void {
+    this.IsActive = !this.IsActive;
+  };
 }
