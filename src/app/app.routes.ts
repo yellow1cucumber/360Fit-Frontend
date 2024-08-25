7726973a3ba62eae133563706cb3e1712adfbffb
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-25 15:48:04 +0500

    FEAT: add nested routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import {HeaderComponent} from "./Components/header/header.component";
 import {LoginViewComponent} from "./Views/login-view/login-view.component";
 
 export const routes: Routes = [
-  {title: "Главная",      path: "",             component: HeaderComponent},
-  {title: "Клиенты",      path: "clients",      component: HeaderComponent},
-  {title: "Услуги",       path: "service",      component: HeaderComponent},
-  {title: "Склад",        path: "storage",      component: HeaderComponent},
-  {title: "Номенклатура", path: "nomenclature", component: HeaderComponent},
-  {title: "Сотрудники",   path: "staff",        component: HeaderComponent},
-  {title: "Расходы",      path: "taxes",        component: HeaderComponent},
-  {title: "Отчеты",       path: "reports",      component: HeaderComponent},
-];
+  {path: "main", children: [
+      {title: "Главная",      path: "",             component: HeaderComponent},
+      {title: "Клиенты",      path: "clients",      component: HeaderComponent},
+      {title: "Услуги",       path: "service",      component: HeaderComponent},
+      {title: "Склад",        path: "storage",      component: HeaderComponent},
+      {title: "Номенклатура", path: "nomenclature", component: HeaderComponent},
+      {title: "Сотрудники",   path: "staff",        component: HeaderComponent},
+      {title: "Расходы",      path: "taxes",        component: HeaderComponent},
+      {title: "Отчеты",       path: "reports",      component: HeaderComponent},
+    ]},
+  {title: "Авторизация",    path: "login",      component: LoginViewComponent},
+  ];
