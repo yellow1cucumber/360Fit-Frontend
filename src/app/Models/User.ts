2536ea3ab1d1e5c9cbf2b433f36e50f7b5800643
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-27 02:26:51 +0500

    FEAT: add new fields to user

--- src/app/Models/User.ts
+++ src/app/Models/User.ts
@@ -2,7 +2,11 @@ export interface User {
   Id: number;
   Name: string;
   Surname: string;
+  Patronymic: string;
+  CardNumber: string;
   Phone: string;
+  Email: string;
+  Deposit: number;
   HasActiveSubscription: boolean
   LastVisit: Date;
 }
--- src/app/Services/Users/users-mock.service.ts
+++ src/app/Services/Users/users-mock.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {BehaviorSubject, filter, map, Observable, of, ReplaySubject} from "rxjs";
 import {User} from "../../Models/User";
 import {IUsersService} from "./i-users.service";
@@ -10,38 +10,185 @@ export class UsersMockService implements IUsersService {
   private selectedUserSubject: ReplaySubject<User> = new ReplaySubject<User>();
   private selectedUser$: Observable<User> = this.selectedUserSubject.asObservable();
 
-  constructor() { }
+  constructor() {
+  }
 
-    public GetUserById(id: number): Observable<User> {
-      return this.users.pipe(
-        map(users => users.find(user => user.Id === id)),
-        filter((user): user is User => !!user)
-      );
-    }
-    public GetUsers(): Observable<User[]> {
-        return this.users;
-    }
+  public GetUserById(id: number): Observable<User> {
+    return this.users.pipe(
+      map(users => users.find(user => user.Id === id)),
+      filter((user): user is User => !!user)
+    );
+  }
 
-    public SetSelectedUser(user: User): void {
-      this.selectedUserSubject.next(user);
-    }
-    public GetSelectedUser(): Observable<User> {
-      return this.selectedUser$;
-    }
+  public GetUsers(): Observable<User[]> {
+    return this.users;
+  }
+
+  public SetSelectedUser(user: User): void {
+    this.selectedUserSubject.next(user);
+  }
+
+  public GetSelectedUser(): Observable<User> {
+    return this.selectedUser$;
+  }
 
   private users: Observable<User[]> = of([
-    {Id: 0, Name: "Никита", Surname: "Садовский", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 1, Name: "Сардар", Surname: "Абдразимов", Phone: "+7 (771) 385-15-74", HasActiveSubscription: false, LastVisit: new Date("2024-08-16")},
-    {Id: 2, Name: "Нурзат", Surname: "Керимбай", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 3, Name: "Маргарита", Surname: "Кожевникова", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 4, Name: "Бекбулат", Surname: "Матенов", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 5, Name: "Нигара", Surname: "Миноварова", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 6, Name: "Сергей", Surname: "Ни", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 7, Name: "Олжас", Surname: "Онгарбек", Phone: "+7 (771) 385-15-74", HasActiveSubscription: false, LastVisit: new Date("2024-08-16")},
-    {Id: 8, Name: "Андрей", Surname: "Пинаев", Phone: "+7 (771) 385-15-74", HasActiveSubscription: false, LastVisit: new Date("2024-08-16")},
-    {Id: 9, Name: "Асиф", Surname: "Рустамоф", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 10, Name: "Аслан", Surname: "Салманов", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 11, Name: "Назгул", Surname: "Талгаткызы", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
-    {Id: 12, Name: "Анна", Surname: "Фролова", Phone: "+7 (771) 385-15-74", HasActiveSubscription: true, LastVisit: new Date("2024-08-16")},
+    {
+      Id: 0,
+      Name: "Никита",
+      Surname: "Садовский",
+      Patronymic: "Алексеевич",
+      CardNumber: "1234-5678-9012-3456",
+      Phone: "+7 (771) 385-15-74",
+      Email: "nikita.sadovsky@example.com",
+      Deposit: 1500,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 1,
+      Name: "Сардар",
+      Surname: "Абдразимов",
+      Patronymic: "Маратович",
+      CardNumber: "2345-6789-0123-4567",
+      Phone: "+7 (771) 385-15-75",
+      Email: "sardar.abdrazimov@example.com",
+      Deposit: 0,
+      HasActiveSubscription: false,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 2,
+      Name: "Нурзат",
+      Surname: "Керимбай",
+      Patronymic: "Жолдасбековна",
+      CardNumber: "3456-7890-1234-5678",
+      Phone: "+7 (771) 385-15-76",
+      Email: "nurzat.kerimbay@example.com",
+      Deposit: 3000,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 3,
+      Name: "Маргарита",
+      Surname: "Кожевникова",
+      Patronymic: "Андреевна",
+      CardNumber: "4567-8901-2345-6789",
+      Phone: "+7 (771) 385-15-77",
+      Email: "margarita.kozhevnikova@example.com",
+      Deposit: 500,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 4,
+      Name: "Бекбулат",
+      Surname: "Матенов",
+      Patronymic: "Бахтиярович",
+      CardNumber: "5678-9012-3456-7890",
+      Phone: "+7 (771) 385-15-78",
+      Email: "bekbulat.matenov@example.com",
+      Deposit: 2500,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 5,
+      Name: "Нигара",
+      Surname: "Миноварова",
+      Patronymic: "Исмаиловна",
+      CardNumber: "6789-0123-4567-8901",
+      Phone: "+7 (771) 385-15-79",
+      Email: "nigara.minovarova@example.com",
+      Deposit: 700,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 6,
+      Name: "Сергей",
+      Surname: "Ни",
+      Patronymic: "Владимирович",
+      CardNumber: "7890-1234-5678-9012",
+      Phone: "+7 (771) 385-15-80",
+      Email: "sergey.ni@example.com",
+      Deposit: 1000,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 7,
+      Name: "Олжас",
+      Surname: "Онгарбек",
+      Patronymic: "Ерланович",
+      CardNumber: "8901-2345-6789-0123",
+      Phone: "+7 (771) 385-15-81",
+      Email: "olzhas.ongarbek@example.com",
+      Deposit: 0,
+      HasActiveSubscription: false,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 8,
+      Name: "Андрей",
+      Surname: "Пинаев",
+      Patronymic: "Григорьевич",
+      CardNumber: "9012-3456-7890-1234",
+      Phone: "+7 (771) 385-15-82",
+      Email: "andrey.pinaev@example.com",
+      Deposit: 0,
+      HasActiveSubscription: false,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 9,
+      Name: "Асиф",
+      Surname: "Рустамов",
+      Patronymic: "Шамильевич",
+      CardNumber: "0123-4567-8901-2345",
+      Phone: "+7 (771) 385-15-83",
+      Email: "asif.rustamov@example.com",
+      Deposit: 500,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 10,
+      Name: "Аслан",
+      Surname: "Салманов",
+      Patronymic: "Рамазанович",
+      CardNumber: "1234-5678-9012-3456",
+      Phone: "+7 (771) 385-15-84",
+      Email: "aslan.salmanov@example.com",
+      Deposit: 2000,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 11,
+      Name: "Назгул",
+      Surname: "Талгаткызы",
+      Patronymic: "Нурлановна",
+      CardNumber: "2345-6789-0123-4567",
+      Phone: "+7 (771) 385-15-85",
+      Email: "nazgul.talgatkyzy@example.com",
+      Deposit: 300,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    },
+    {
+      Id: 12,
+      Name: "Анна",
+      Surname: "Фролова",
+      Patronymic: "Сергеевна",
+      CardNumber: "3456-7890-1234-5678",
+      Phone: "+7 (771) 385-15-86",
+      Email: "anna.frolova@example.com",
+      Deposit: 1000,
+      HasActiveSubscription: true,
+      LastVisit: new Date("2024-08-16")
+    }
   ]);
+
 }
