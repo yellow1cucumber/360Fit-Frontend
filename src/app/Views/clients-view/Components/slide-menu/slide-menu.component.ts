bde49dbf0ed1952eb8de7f45179da9ec5d08d9ea
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-26 21:42:00 +0500

    FEAT: connect selected user$ with slide menu

--- src/app/Views/clients-view/Components/slide-menu/slide-menu.component.ts
+++ src/app/Views/clients-view/Components/slide-menu/slide-menu.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {NgClass} from "@angular/common";
+import {USERS_SERVICE_TOKEN} from "../../../../Services/InjectionTokens";
+import {IUsersService} from "../../../../Services/Users/i-users.service";
+import {User} from "../../../../Models/User";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-slide-menu',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    FormsModule
   ],
   templateUrl: './slide-menu.component.html',
   styleUrl: './slide-menu.component.scss'
 })
 export class SlideMenuComponent {
+  constructor(@Inject(USERS_SERVICE_TOKEN) private usersService: IUsersService) {
+    this.usersService.GetSelectedUser().subscribe(
+      value => this.ActiveUser = value,
+    )
+  }
+  public ActiveUser: User | null = null;
 }
