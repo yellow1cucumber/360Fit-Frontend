ac8bb8a61c55514271f2ab989abe2ff59ab1a07b
yellow1cucumber <sadovskiy1n@gmail.com>
2024-08-26 22:15:56 +0500

    FEAT: connect form

--- src/app/Views/clients-view/Components/slide-menu/slide-menu.component.ts
+++ src/app/Views/clients-view/Components/slide-menu/slide-menu.component.ts
@@ -3,14 +3,15 @@ import {NgClass} from "@angular/common";
 import {USERS_SERVICE_TOKEN} from "../../../../Services/InjectionTokens";
 import {IUsersService} from "../../../../Services/Users/i-users.service";
 import {User} from "../../../../Models/User";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-slide-menu',
   standalone: true,
   imports: [
     NgClass,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './slide-menu.component.html',
   styleUrl: './slide-menu.component.scss'
@@ -18,8 +19,22 @@ import {FormsModule} from "@angular/forms";
 export class SlideMenuComponent {
   constructor(@Inject(USERS_SERVICE_TOKEN) private usersService: IUsersService) {
     this.usersService.GetSelectedUser().subscribe(
-      value => this.ActiveUser = value,
+      (value) => {
+        this.ActiveUser = value;
+        console.log(this.ActiveUser);
+      },
     )
   }
   public ActiveUser: User | null = null;
+
+  public UserFormGroup: FormGroup = new FormGroup({
+    'Name': new FormControl('', Validators.required),
+    'Surname': new FormControl('', Validators.required),
+    'Patronymic': new FormControl('', Validators.required),
+
+    'CardNumber': new FormControl('', Validators.required),
+    'PhoneNumber': new FormControl('', Validators.pattern("[0-9]{10}")),
+    'Email': new FormControl('', Validators.email),
+    'DateOfBirth': new FormControl('', Validators.required),
+  });
 }
